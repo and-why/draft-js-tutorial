40153bbdd2e87ca6652bd18399d393d1663ec1e8
Andy Smith <asmith@AFA.private>
2018-10-04 16:04:00 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { EditorState, Editor, RichUtils, convertToRaw, convertFromRaw  } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
 
-class App extends Component {
+class DraftEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+
+    const content = window.localStorage.getItem('content')
+
+    if(content) {
+      
+      const convertedState = convertFromRaw(JSON.parse(content))
+      console.log(convertedState)
+      this.state.editorState = EditorState.createWithContent(convertedState);
+      this.state.notes = stateToHTML(convertedState)
+      console.log(JSON.parse(content))
+      
+    } else {
+      this.state.editorState = EditorState.createEmpty();
+    }
+
+  }
+
+  onChange = (editorState) => {
+    const contentState = editorState.getCurrentContent();
+    console.log('content state: ', convertToRaw(contentState))
+    
+    this.setState({
+      editorState,
+      notes: stateToHTML(editorState.getCurrentContent())
+    });
+  }
+  saveContent = () => {
+    const contentState = this.state.editorState.getCurrentContent();;
+    window.localStorage.setItem('content', JSON.stringify(convertToRaw(contentState)))
+    // let notes = JSON.stringify(convertToRaw(content))
+    // this.setState({notes})
+  }
+  
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    
+    if(newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    
+    return 'not-handled';
+  }
+  
+  onUnderlineClick = () => {
+     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+  onBoldClick = () => {
+     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  onItalicClick = () => {
+     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+  onToggleCode = () => {
+     this.onChange(RichUtils.toggleCode(this.state.editorState));
+  }
+
+  
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.onUnderlineClick}>U</button>
+        <button onClick={this.onBoldClick}>B</button>
+        <button onClick={this.onItalicClick}>I</button>
+        <button onClick={this.onToggleCode}>Code Block</button>
+          <Editor
+            handleKeyCommand={this.handleKeyCommand}
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+          <button onClick={this.saveContent}>Save Data</button>
+
+        
+        <p dangerouslySetInnerHTML={{__html: this.state.notes}} />
       </div>
     );
   }
 }
 
-export default App;
+export default DraftEditor;
